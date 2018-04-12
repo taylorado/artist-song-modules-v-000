45231496f1b33a2824914381728897dee0f19ab9
Taylor Donlan <taylordonlan@gmail.com>
2018-04-12 18:05:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  #include Paramable
+  include Paramable
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
