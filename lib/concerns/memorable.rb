97608788af8a7eb8baa842b46bf0df1930d55185
Taylor Donlan <taylordonlan@gmail.com>
2018-04-12 17:56:02 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -10,9 +10,9 @@ module Memorable
   end
 
   module InstanceMethods
-    def initialize
-      self.class.all << self
-    end
+  #  def initialize
+  #    self.class.all << self
+  #  end
   end
 
 end
