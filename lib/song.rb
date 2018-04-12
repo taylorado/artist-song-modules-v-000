f151cbb1774f081e32ecf988a75b7f6704719502
Taylor Donlan <taylordonlan@gmail.com>
2018-04-12 18:05:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,16 +4,16 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  #include Paramable
+  include Paramable
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  #def initialize
+  #  @@songs << self
+  #end
 
   #def self.find_by_name(name)
   #  @@songs.detect{|a| a.name == name}
