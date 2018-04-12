d101b3d2a54c7b089a82baba27c972dd371876e4
Taylor Donlan <taylordonlan@gmail.com>
2018-04-12 17:56:34 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,5 @@
 module Memorable
-  module ClassMethods
+  #module ClassMethods
     def reset_all
       all.clear
     end
@@ -7,12 +7,12 @@ module Memorable
     def count
       all.count
     end
-  end
+  #end
 
-  module InstanceMethods
+  #module InstanceMethods
   #  def initialize
   #    self.class.all << self
   #  end
-  end
+  #end
 
 end
